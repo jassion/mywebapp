136b1af88fffa90bfe5cffbf2dc287c7a365aafe
jassionzhao <jassionzhao@zhaoxin.com>
2018-09-30 13:18:26 +0800

    add some comment for app.py
    Signed-off-by: jassionzhao <jassionzhao@zhaoxin.com>

--- www/app.py
+++ www/app.py
@@ -18,6 +18,7 @@ from jinja2 import Environment, FileSystemLoader
 import orm
 from coroweb import add_routes, add_static
 
+# jinja2是模板引擎，主要是对模板的配置和使用
 def init_jinja2(app, **kw):
     logging.info('init jinja2...')
     options = dict(
@@ -37,7 +38,7 @@ def init_jinja2(app, **kw):
     if filters is not None:
         for name, f in filters.items():
             env.filters[name] = f
-    app['__templating__'] = env
+    app['__templating__'] = env # 将配置好的模板使用环境传给app的'__templating__'属性
 
 async def logger_factory(app, handler):
     async def logger(request):
@@ -47,17 +48,21 @@ async def logger_factory(app, handler):
     return logger
 
 async def data_factory(app, handler):
-    async def parse_dara(request):
+    async def parse_data(request): # 解析request，将解析得到的数据放在request的__data__属性中
         if request.method == 'POST':
-            if request.content_type.startswith('application/json'):
+            if request.content_type.startswith('application/json'): # 若request的content_type是json，则用request.json()来解析
                 request.__data__ = await request.json()
                 logging.info('request json: %s' % str(request.__data__))
-            elif request.content_type.startswith('application/x-www-form-urlencoded'):
+            elif request.content_type.startswith('application/x-www-form-urlencoded'): # 若request的content_type是x-www-form-urlencoded，则用request.post()来解析
                 request.__data__ = await request.post()
                 logging.info('request form: %s' % str(request.__data__))
         return (await handler(request))
     return parse_data
 
+'''
+response_factory生成响应response，并返回，交给aiohttp server去发送给请求者
+根据 handler(request)返回的结果来判断如何生成需要的响应response
+'''
 async def response_factory(app, handler):
     async def response(request):
         logging.info('Response handler...')
@@ -76,23 +81,23 @@ async def response_factory(app, handler):
             resp = web.Response(body=r.encode('utf-8'))
             resp.content_type = 'text/html;charset=utf-8'
             return resp
-        if isinstance(r, dict):
-            template = r.get('__template__')
+        if isinstance(r, dict): # 主要返回的大部分是dict，执行该项
+            template = r.get('__template__') # 获取handler返回的dict中的__template__属性
             if template is None:
                 resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
                 resp.content_type = 'application/json;charset=utf-8'
                 return resp
             else:
-                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
-                resp.content_type = 'text/html;charset=utf-8'
-                return resp
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8')) # 从配置好的template环境中获取对应的template
+                resp.content_type = 'text/html;charset=utf-8' # jinja2的Environment对象通过get_template(template)获取一个具体的模板文件，
+                return resp      # 模板文件通过.render(params)接收参数，并且对模板进行渲染，这里的渲染就是将模板中对应的变量根据传入的参数进行赋值处理成静态的html文件
         if isinstance(r, int) and r >= 100 and r < 600:
             return web.Response(r)
         if isinstance(r, tuple) and len(r) == 2:
             t, m = r
             if isinstance(t, int) and t >= 100 and t < 600:
                 return web.Response(t, str(m))
-        # default:
+        # default: 将r当作str处理
         resp = web.Response(body=str(r).encode('utf-8'))
         resp.content_type = 'text/plain;charset=utf-8'
         return resp
